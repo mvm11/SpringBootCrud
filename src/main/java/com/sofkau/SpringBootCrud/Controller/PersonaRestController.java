ef213c60b946ffa3a2e63767ec60893ac49fb572
mvm11 <mvalenciaminota@gmail.com>
2021-09-11 16:40:55 -0500

    Se define la interface que el controlador va a llamar

---
+++ src/main/java/com/sofkau/SpringBootCrud/Controller/PersonaRestController.java
@@ -0,0 +1,11 @@
+package com.sofkau.SpringBootCrud.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/personas")
+public class PersonaRestController {
+
+
+}
---
+++ src/main/java/com/sofkau/SpringBootCrud/Services/InterfaceServicesPersona.java
@@ -0,0 +1,14 @@
+package com.sofkau.SpringBootCrud.Services;
+
+import com.sofkau.SpringBootCrud.Entity.Persona;
+
+import java.util.List;
+
+public interface InterfaceServicesPersona {
+
+    public List<Persona> List();
+    public Persona ListById(int id);
+    public Persona save(Persona persona);
+    public void delete(int id);
+    public Persona update(Persona persona);
+}
